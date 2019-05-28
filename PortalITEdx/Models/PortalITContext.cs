d180e1db5025f83c18de01f5d81d8bf216297175
hugofe <jh.ferreirati@gmail.com>
2019-05-28 09:51:04 -0300

    Initial Structure

---
+++ PortalITEdx/Models/PortalITContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+
+namespace PortalITEdx.Models
+{
+    public partial class PortalITContext : DbContext
+    {
+        public PortalITContext()
+        {
+        }
+
+        public PortalITContext(DbContextOptions<PortalITContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json");
+                var configuration = builder.Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+        }
+    }
+}
